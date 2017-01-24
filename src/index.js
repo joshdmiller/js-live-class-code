fc60ea12377be0070d753e9fb330dc3a85182a3b
Josh David Miller <josh@joshdmiller.com>
2017-01-23 19:14:43 -0800

    feat: render todos in browser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-/**
-* Put your JavaScript here
-*/
+function onReady () {
+  const addTodoForm = document.getElementById( 'addTodoForm' );
+  const todoList = document.getElementById( 'todoList' );
+  const newTodoText = document.getElementById( 'newTodoText' );
 
-console.log( 'Open the dev console to see me. :-)' );
+  // <ul></ul>
+  todoList.textContent = '';
+
+  addTodoForm.addEventListener( 'submit', event => {
+    event.preventDefault();
+    const title = newTodoText.value;
+
+    // add it to the list
+    // create an li
+    // <li>{title}</li>
+    const newLi = document.createElement( 'li' );
+    newLi.textContent = title; // vs newLi.innerHTML
+
+    newLi.addEventListener( 'click', () => {
+      newLi.classList.toggle( 'todo--complete' );
+    });
+
+    // put it in the ul
+    todoList.appendChild( newLi );
+
+    newTodoText.value = '';
+  });
+}
+
+if ( document.readyState !== 'loading' ) {
+  onReady();
+} else {
+  document.addEventListener( 'DOMContentLoaded', onReady );
+}
 
