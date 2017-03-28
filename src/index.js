33206d3e60e52feaa7bd3932e9736d2f0314ba68
Josh David Miller <josh@joshdmiller.com>
2017-03-27 17:08:12 -0700

    feat: port frontend to vue

--- src/index.js
+++ src/index.js
@@ -1,51 +1,37 @@
+import Vue from 'vue/dist/vue.esm.js';
 import TodoApp from './TodoApp';
 
 function onReady () {
   const app = TodoApp();
 
-  const addTodoForm = document.getElementById( 'addTodoForm' );
-  const todoList = document.getElementById( 'todoList' );
-  const newTodoText = document.getElementById( 'newTodoText' );
-  const toggleBtn = document.getElementById( 'toggleBtn' );
+  const vueApp = new Vue({
+    el: '#app',
+    data: {
+      newTodoText: '',
+      todos: app.getTodos(),
+    },
+    methods: {
+      toggleComplete ( todo ) {
+        app.toggleComplete( todo.getId() );
+      },
 
-  addTodoForm.addEventListener( 'submit', event => {
-    event.preventDefault();
-    const title = newTodoText.value;
+      toggleFilter () {
+        app.toggleFilter();
+      },
 
-    app.addTodo( title );
-
-    newTodoText.value = '';
+      addTodo () {
+        app.addTodo( this.newTodoText );
+        this.newTodoText = '';
+      },
+    },
   });
-
-  toggleBtn.addEventListener( 'click', () => app.toggleFilter() );
 
   // ( state ) => ui
   const renderTheUi = ( todos ) => {
-    // <ul></ul>
-    todoList.textContent = '';
-
-    todos.forEach( todo => {
-      // add it to the list
-      // create an li
-      // <li>{title}</li>
-      const newLi = document.createElement( 'li' );
-      newLi.textContent = todo.getTitle(); // vs newLi.innerHTML
-
-      if ( todo.isComplete() ) {
-        newLi.classList.add( 'todo--complete' );
-      }
-
-      newLi.addEventListener( 'click', () => {
-        app.toggleComplete( todo.getId() );
-      });
-
-      // put it in the ul
-      todoList.appendChild( newLi );
-    });
+    vueApp.todos = todos;
   };
 
   app.subscribe( () => renderTheUi( app.getTodos() ) );
-  renderTheUi( app.getTodos() );
 }
 
 if ( document.readyState !== 'loading' ) {
--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
       {
         test: /\.js$/,
         loaders: [ 'babel-loader' ],
-        include: path.join( __dirname, 'src' ),
+        // include: path.join( __dirname, 'src' ),
       },
     ],
   }
